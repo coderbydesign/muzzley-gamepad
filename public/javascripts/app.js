53eb7a6b76b6ecd81d8f2102686a5cdaa341ee69
Keith Walsh <keith.walsh@adtegrity.com>
2014-09-16 09:01:00 -0400

    Moved CSS/JS out into dedicated files

---
+++ public/javascripts/app.js
@@ -0,0 +1,32 @@
+$(function(){
+  var token = '467c85cccaacf5a1';
+
+  muzzley.on('error', function(e) {
+    $('#error').text(e).show();
+  });
+
+  muzzley.connectApp(token, function(e, activity) {
+    if (e) return $('#error').text(e).show();
+
+    qrUrl = activity.qrCodeUrl
+    $('#qrCnt').attr('src', qrUrl);
+    $('#loader').hide();
+
+    activity.on('participantQuit', function(participant) {
+      $('#action').text('Check ya later!');
+      $('#qrCnt').show().attr('src', qrUrl);
+    });
+
+    activity.on('participantJoin', function(participant) {
+      participant.changeWidget('gamepad', function(e) {
+        if (e) return $('#error').text(e).show();
+        $('#action').text('Howdy!');
+        $('#qrCnt').hide();
+      });
+
+      participant.on('action', function(action) {
+        $('#action').text(JSON.stringify(action));
+      });
+    });
+  });
+});
